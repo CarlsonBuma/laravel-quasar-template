facacb22b245db017580d41d6f7e1c4fd9d278e4
CarlsonBuma <75643791+CarlsonBuma@users.noreply.github.com>
2024-11-23 17:05:01 +0100

    ui navigation improvements -v.0.4.2

--- frontend/src/components/navigation/NavEntity.vue
+++ frontend/src/components/navigation/NavEntity.vue
@@ -1,14 +1,20 @@
 <template>
-
-    <q-tabs class="text-weight-regular" active-color="primary" inline-label>
-        <q-route-tab to="/entity/dashboard" label="My Cockpit" />
-        <q-route-tab to="/entity/profile" label="Profile" />
-    </q-tabs>
+    <q-toolbar>
+        <span>{{ title ?? 'My Cockpit' }}</span>
+        <q-space />
+        <q-tabs class="text-weight-regular" active-color="primary" shrink stretch>
+            <q-route-tab to="/entity/dashboard" label="My Cockpit" />
+            <q-route-tab to="/entity/profile" label="Profile" />
+        </q-tabs>
+    </q-toolbar>
 
 </template>
 
 <script>
 export default {
     name: 'NavEntity',
+    props: {
+        title: String,
+    },
 };
 </script>
--- frontend/src/components/navigation/NavUser.vue
+++ frontend/src/components/navigation/NavUser.vue
@@ -1,28 +1,30 @@
 <template>
 
-    <q-tabs class="text-weight-regular" active-color="primary" inline-label >
-        <q-route-tab to="/user/dashboard" exact label="Dashboard" />
-        <q-btn-dropdown auto-close stretch flat label="My avatar">
-            <q-list>
-                <q-item clickable @click="$router.push('/account/access')">
-                    <q-item-section>My access</q-item-section>
-                </q-item>
-                <q-item clickable @click="$router.push('/account/settings')">
-                    <q-item-section>Settings</q-item-section>
-                </q-item>
-            </q-list>
-        </q-btn-dropdown>
-    </q-tabs>
+    <q-toolbar>
+        <span>{{ title ?? 'My Avatar' }}</span>
+        <q-space />
+        <q-tabs class="text-weight-regular" active-color="primary" shrink stretch>
+            <q-route-tab to="/user/dashboard" exact label="Dashboard" />
+            <q-btn-dropdown auto-close stretch flat label="My profile">
+                <q-list>
+                    <q-item clickable @click="$router.push('/account/access')">
+                        <q-item-section>My access</q-item-section>
+                    </q-item>
+                    <q-item clickable @click="$router.push('/account/settings')">
+                        <q-item-section>Settings</q-item-section>
+                    </q-item>
+                </q-list>
+            </q-btn-dropdown>
+        </q-tabs>
+    </q-toolbar>
 
 </template>
 
 <script>
 export default {
     name: 'NavUser',
-    setup() {
-        return {
-            // 
-        };
+    props: {
+        title: String,
     },
 };
 </script>
