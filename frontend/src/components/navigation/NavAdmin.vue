4f7372b37fa5180be7b15fc2c7fca452278d3d8e
CarlsonBuma <75643791+CarlsonBuma@users.noreply.github.com>
2024-11-23 17:20:15 +0100

    ui improvements -v.0.4.4

--- frontend/src/components/navigation/NavAdmin.vue
+++ frontend/src/components/navigation/NavAdmin.vue
@@ -1,18 +1,21 @@
 <template>
 
-    <q-tabs
-        class="text-weight-regular"
-        active-color="primary"
-        inline-label
-    >
-        <q-route-tab to="/admin/dashboard" exact label="Dashboard" />
-        <q-route-tab to="/admin/newsfeed" exact label="Newsfeed" />
-    </q-tabs>
+    <q-toolbar>
+        <span class="gt-xs">{{ title ?? 'Backpanel' }}</span>
+        <q-space />
+        <q-tabs class="text-weight-regular" active-color="primary" shrink stretch>
+            <q-route-tab to="/admin/dashboard" exact label="Dashboard" />
+            <q-route-tab to="/admin/newsfeed" exact label="Newsfeed" />
+        </q-tabs>
+    </q-toolbar>
 
 </template>
 
 <script>
 export default {
     name: 'NavAdmin',
+    props: {
+        title: String,
+    },
 };
 </script>
